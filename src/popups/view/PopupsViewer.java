22619ce72ceb0869297f69ac0bcb40420a3cfed6
thenaz <anaz7963@740s-pl-126.local>
2016-09-27 08:35:16 -0600

    Put input with wacks and stars
    9/27

--- src/popups/view/PopupsViewer.java
+++ src/popups/view/PopupsViewer.java
@@ -16,4 +16,19 @@ public class PopupsViewer
 	{
 		JOptionPane.showMessageDialog(null, message);
 	}
+	
+	/**
+	 * Creates an input poput to collect a response from the user as a String.
+	 * @param question The question being asked to the user.
+	 * @return The user's response.
+	 */
+	
+	public String collectResponse(String question)
+	{
+		String response = "";
+		
+		response = JOptionPane.showInputDialog(null, question);
+		
+		return response;
+	}
 }
