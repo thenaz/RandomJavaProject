f7b14d54678c3c83764facf39c4ccf2d7bdf321c
thenaz <anaz7963@740s-pl-126.local>
2016-09-27 09:50:29 -0600

    Finished popups
    9/27

--- src/popups/controller/PopupsController.java
+++ src/popups/controller/PopupsController.java
@@ -1,6 +1,21 @@
 package popups.controller;
 
+import popups.view.PopupsViewer;
+
 public class PopupsController
 {
-
+	
+	private PopupsViewer display;
+	
+	public PopupsController()
+	{
+		display = new PopupsViewer();
+	}
+	
+	public void start()
+	{
+		display.displayMessage("Hi there :D");
+		String answer = display.collectResponse("What is going on?");
+		display.displayMessage(answer + "is what you said.");
+	}
 }
