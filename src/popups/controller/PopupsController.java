1c3725df9fd3368e8e815dfde17b698273c69958
thenaz <anaz7963@740s-pl-126.local>
2016-09-29 08:25:26 -0600

    Added ints and doubles
    9/29

--- src/popups/controller/PopupsController.java
+++ src/popups/controller/PopupsController.java
@@ -14,8 +14,36 @@ public class PopupsController
 	
 	public void start()
 	{
+		String answer = "sample";
+		while(answer != null && !answer.equals(""));
+	}
+	
+	{
 		display.displayMessage("Hi there :D");
 		String answer = display.collectResponse("What is going on?");
 		display.displayMessage(answer + "is what you said.");
+	}
+
+	
+	/**
+	 * Checks if the supplied String can be parsed to a double value.
+	 * @param potentialValue The string to test.
+	 * @return Whether the value could be parsed; true if it can be parsed, false otherwise.
+	 */
+	private boolean isDouble(String potentialValue)
+	{
+	boolean isParseable = false;
+	
+	try
+	{
+		double test = Double.parseDouble(potentialValue);
+		isParseable = true;
+	}
+	catch(NumberFormatException notDouble)
+	{
+		display.displayMessge("This is not a double value =:<");
+	}
+	
+	return isParseable;
 	}
 }
--- src/popups/model/PopupsThingy.java
+++ src/popups/model/PopupsThingy.java
@@ -2,5 +2,39 @@ package popups.model;
 
 public class PopupsThingy
 {
-
+	private int number;
+	private double value;
+    private String words;
+    private boolean isValid;
+    
+    public String getWords;
+    public int getNumber()
+    {
+    	return number;
+    }
+    public double getValue()
+    {
+    	return value;
+    }
+    public boolean isValid()
+    {
+    	return isValid;
+    }
+    
+    public void setWords(String words)
+    {
+    	this.words = words;
+    }
+    public void setNumber(int number)
+    {
+    	this.number = number;
+    }
+    public void setValue(double value)
+    {
+    	this.value = value;
+    }
+    public void setValid(boolean isValid)
+    {
+    	this.isValid = isValid;
+    }
 }
